264a36405fc735293d1a76e7d9e3bad8f5360014
SlavenaS <slavena1010@gmail.com>
2024-04-16 09:42:14 +0300

    tree1

---
+++ Trees_Zaimov_SS/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Trees_Zaimov_SS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 34);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 77);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name_BG";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 118);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 165);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 53);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Insert Into type";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(236, 258);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Trees_Zaimov_SS/Form1.cs
@@ -0,0 +1,76 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Trees_Zaimov_SS
+{
+    public partial class Form1 : Form
+    {
+        string connstr = "server=10.6.0.127;" + "port=3306;"
+              + "user=PC1;" + "password=1111;" + "database=trees_zaimov;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection connect = new MySqlConnection(connstr);
+            if (connect.State == 0)
+            {
+                connect.Open();
+            }
+            MessageBox.Show("Connection NOW opened");
+
+            string str = "select * from type";
+            MySqlCommand query = new MySqlCommand(str, connect);
+            MySqlDataReader reader = query.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader[0]} {reader[1]}");
+            }
+            reader.Close();
+            connect.Close();
+            //type-vid
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string insert1 = 
+              "INSERT INTO trees_zaimov.type " +
+              "(`name`, `name_bg`) " +
+              "VALUES (@name, @name_bg)";
+
+            MySqlConnection connect = new MySqlConnection(connstr);
+            if (connect.State == 0)
+            {
+                connect.Open();
+            }
+          
+            MySqlCommand query = new MySqlCommand(insert1, connect);
+            query.Parameters.AddWithValue("@name", textBox1.Text);
+            query.Parameters.AddWithValue("@name_bg", textBox2.Text);
+            int result = query.ExecuteNonQuery();
+            if(result != 0)
+            {
+                MessageBox.Show($"Dobaveni sa {result} br. records in type table!!! ");
+            }
+            else
+            {
+
+                MessageBox.Show("nesto ne e nared in type table!!! ");
+
+            }
+            connect.Close();
+        }
+    }
+}
